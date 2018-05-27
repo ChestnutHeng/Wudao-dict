74e3a864cbd3b1e82d655e639bace6f495e4d39b
明明 <erickwok404@gmail.com>
2018-05-27 10:30:40 +0800

    修改了start_gui.sh，现在启动图形界面时会检测是否启动了WudaoServer，若没有，则启动

--- wudao-dict/start_gui.sh
+++ wudao-dict/start_gui.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+count=`ps -ef | grep "python3 WudaoServer.py" | grep -v "grep" | wc -l`
+ 
+if [ $count == 0 ]; then
+    nohup python3 WudaoServer.py > ./usr/server.log 2>&1 &
+    git pull origin master > ./usr/pull.log 2>&1 &
+fi
 python3 ./mainwindow.py
