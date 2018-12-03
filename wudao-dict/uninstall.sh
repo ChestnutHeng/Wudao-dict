4320ef0a45b7f9d5c6b0098c32f0aba233c7743a
Beijixiong <beijixiong@pinduoduo.com>
2018-12-03 17:12:27 +0800

    add Mac OS support

--- wudao-dict/uninstall.sh
+++ wudao-dict/uninstall.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 wd -k
-# 删除系统命令wd
-sudo rm -f /usr/bin/wd
 
-# 删除自动补全
-sudo rm -f /etc/bash_completion.d/wd
+sysOS=`uname -s`
+if [ $sysOS == "Darwin" ];then
+    # 删除系统命令wd
+    sudo rm -f /usr/local/bin/wd
+    # 删除自动补全
+    sudo rm -f /usr/local/etc/bash_completion.d/wd
+else
+    # 删除系统命令wd
+    sudo rm -f /usr/bin/wd
+    # 删除自动补全
+    sudo rm -f /etc/bash_completion.d/wd
+fi
 
 echo 'Uninstall Finished! '
 
